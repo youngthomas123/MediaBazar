00e61db902e25514fac5c04222bc360b4426268c
Bruno Mella <507479@student.fontys.nl>
2023-03-06 16:21:25 +0200

    Log In Form

--- S2GroupProject/Form1.Designer.cs
+++ S2GroupProject/Form1.Designer.cs
@@ -28,26 +28,103 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.LoginBT = new System.Windows.Forms.Button();
+            this.ShowPasswordCB = new System.Windows.Forms.CheckBox();
+            this.PasswordTB = new System.Windows.Forms.TextBox();
+            this.UserNameTB = new System.Windows.Forms.TextBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
+            // 
+            // LoginBT
+            // 
+            this.LoginBT.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.LoginBT.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.LoginBT.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.LoginBT.Location = new System.Drawing.Point(182, 350);
+            this.LoginBT.Name = "LoginBT";
+            this.LoginBT.Size = new System.Drawing.Size(380, 48);
+            this.LoginBT.TabIndex = 9;
+            this.LoginBT.Text = "Login";
+            this.LoginBT.UseVisualStyleBackColor = false;
+            // 
+            // ShowPasswordCB
+            // 
+            this.ShowPasswordCB.AutoSize = true;
+            this.ShowPasswordCB.Location = new System.Drawing.Point(288, 306);
+            this.ShowPasswordCB.Name = "ShowPasswordCB";
+            this.ShowPasswordCB.Size = new System.Drawing.Size(162, 29);
+            this.ShowPasswordCB.TabIndex = 8;
+            this.ShowPasswordCB.Text = "Show Password";
+            this.ShowPasswordCB.UseVisualStyleBackColor = true;
+            // 
+            // PasswordTB
+            // 
+            this.PasswordTB.ForeColor = System.Drawing.Color.DarkGray;
+            this.PasswordTB.Location = new System.Drawing.Point(230, 257);
+            this.PasswordTB.Name = "PasswordTB";
+            this.PasswordTB.Size = new System.Drawing.Size(332, 31);
+            this.PasswordTB.TabIndex = 6;
+            this.PasswordTB.UseSystemPasswordChar = true;
+            // 
+            // UserNameTB
+            // 
+            this.UserNameTB.ForeColor = System.Drawing.Color.DarkGray;
+            this.UserNameTB.Location = new System.Drawing.Point(230, 191);
+            this.UserNameTB.Name = "UserNameTB";
+            this.UserNameTB.Size = new System.Drawing.Size(332, 31);
+            this.UserNameTB.TabIndex = 7;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(182, 251);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(42, 37);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(182, 185);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(42, 37);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(338, 82);
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(301, 81);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(62, 25);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Log In";
+            this.label1.Size = new System.Drawing.Size(135, 48);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "LOG IN";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(761, 571);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.LoginBT);
+            this.Controls.Add(this.ShowPasswordCB);
+            this.Controls.Add(this.PasswordTB);
+            this.Controls.Add(this.UserNameTB);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -55,6 +132,12 @@
 
         #endregion
 
+        private Button LoginBT;
+        private CheckBox ShowPasswordCB;
+        private TextBox PasswordTB;
+        private TextBox UserNameTB;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox2;
         private Label label1;
     }
 }
