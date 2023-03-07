b336008e4f1bafa1577db5138e49906efc355998
Panyoo <71757463+Panyoo@users.noreply.github.com>
2023-03-07 15:38:09 +0100

    I dont know xd

---
+++ S2GroupProject/LoginForm.cs
@@ -0,0 +1,34 @@
+using System.Security.Cryptography;
+
+namespace S2GroupProject
+{
+    public partial class LoginForm : Form
+    {
+		
+		public LoginForm()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void UserNameTB_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LoginBT_Click(object sender, EventArgs e)
+        {
+			if (UserNameTB.Text == "Admin" && PasswordTB.Text == "123456")
+			{
+				Form1 form1 = new Form1();
+				form1.Show();
+				this.Hide();
+			}
+			else
+			{
+				MessageBox.Show("Invalid username or password");
+			}
+
+		}
+    }
+}
