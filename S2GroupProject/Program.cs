dfd11df25b248b0f2b9423703670f3ec57a31a5f
Thomas Vasanth <t.vasanththomas@student.fontys.nl>
2023-03-13 09:24:14 +0100

    add

--- S2GroupProject/Program.cs
+++ S2GroupProject/Program.cs
@@ -11,7 +11,7 @@ namespace S2GroupProject
 			// To customize application configuration such as set high DPI settings or default font,
 			// see https://aka.ms/applicationconfiguration.
 			ApplicationConfiguration.Initialize();
-			Application.Run(new LoginForm());
+			Application.Run(new Form1());
 		}
 	}
 }
