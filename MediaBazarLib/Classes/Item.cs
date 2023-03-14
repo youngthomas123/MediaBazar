fe7da2acf9b03c629bad92c5c2bf3797cc1eb43a
Bruno Mella <507479@student.fontys.nl>
2023-03-14 16:56:19 +0200

    resolved merge conflict

--- MediaBazarLib/Classes/Item.cs
+++ MediaBazarLib/Classes/Item.cs
@@ -8,19 +8,12 @@ namespace MediaBazarLib
 {
     public class Item
     {
-<<<<<<< HEAD
-        public Item(string name, string description, int quantity) 
-        {
-            Name= name;
-            Description= description;
-            Quantity= quantity;
-=======
+
         public Item(string name, string description, int quantity)
         {
             Name = name;
             Description = description;
             Quantity = quantity;
->>>>>>> 5e1f3eb16990a817b5939c5f47889f18af6f95a5
         }
 
         public string Name { get; set; }    
