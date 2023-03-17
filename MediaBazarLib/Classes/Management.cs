22f6a74133eb7e13c87efa844e06ccf7d28cecb2
Mihail Mihov <500867@student.fontys.nl>
2023-03-17 10:41:41 +0100

    something

--- MediaBazarLib/Classes/Management.cs
+++ MediaBazarLib/Classes/Management.cs
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
+
 ﻿using MediaBazarLib;
-=======
 ﻿using MediaBazarLib.Classes;
->>>>>>> 5e1f3eb16990a817b5939c5f47889f18af6f95a5
 using System;
 using System.Collections.Generic;
 using System.Globalization;
