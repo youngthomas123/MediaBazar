b66e9c5df6b6cfc23dae3cad3141d989edd5d041
Bruno Mella <507479@student.fontys.nl>
2023-05-26 01:22:35 +0300

    Language feature has been removed

--- S2GroupProject/Forms/LoginForm.Designer.cs
+++ S2GroupProject/Forms/LoginForm.Designer.cs
@@ -1,152 +1,118 @@
 ﻿namespace S2GroupProject
 {
-    partial class LoginForm
-    {
-        /// <summary>
-        ///  Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
+	partial class LoginForm
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        ///  Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
 
-        #region Windows Form Designer generated code
+		#region Windows Form Designer generated code
 
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
-            this.LoginBT = new System.Windows.Forms.Button();
-            this.ShowPasswordCB = new System.Windows.Forms.CheckBox();
-            this.PasswordTB = new System.Windows.Forms.TextBox();
-            this.UserNameTB = new System.Windows.Forms.TextBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
-            this.radioButton3 = new System.Windows.Forms.RadioButton();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // LoginBT
-            // 
-            this.LoginBT.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            resources.ApplyResources(this.LoginBT, "LoginBT");
-            this.LoginBT.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.LoginBT.Name = "LoginBT";
-            this.LoginBT.UseVisualStyleBackColor = false;
-            this.LoginBT.Click += new System.EventHandler(this.LoginBT_Click_1);
-            // 
-            // ShowPasswordCB
-            // 
-            resources.ApplyResources(this.ShowPasswordCB, "ShowPasswordCB");
-            this.ShowPasswordCB.Name = "ShowPasswordCB";
-            this.ShowPasswordCB.UseVisualStyleBackColor = true;
-            // 
-            // PasswordTB
-            // 
-            this.PasswordTB.ForeColor = System.Drawing.Color.DarkGray;
-            resources.ApplyResources(this.PasswordTB, "PasswordTB");
-            this.PasswordTB.Name = "PasswordTB";
-            this.PasswordTB.UseSystemPasswordChar = true;
-            // 
-            // UserNameTB
-            // 
-            this.UserNameTB.ForeColor = System.Drawing.Color.DarkGray;
-            resources.ApplyResources(this.UserNameTB, "UserNameTB");
-            this.UserNameTB.Name = "UserNameTB";
-            // 
-            // pictureBox3
-            // 
-            resources.ApplyResources(this.pictureBox3, "pictureBox3");
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.TabStop = false;
-            // 
-            // pictureBox2
-            // 
-            resources.ApplyResources(this.pictureBox2, "pictureBox2");
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.TabStop = false;
-            // 
-            // label1
-            // 
-            resources.ApplyResources(this.label1, "label1");
-            this.label1.Name = "label1";
-            // 
-            // radioButton1
-            // 
-            resources.ApplyResources(this.radioButton1, "radioButton1");
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.TabStop = true;
-            this.radioButton1.UseVisualStyleBackColor = true;
-            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
-            // 
-            // radioButton2
-            // 
-            resources.ApplyResources(this.radioButton2, "radioButton2");
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.TabStop = true;
-            this.radioButton2.UseVisualStyleBackColor = true;
-            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
-            // 
-            // radioButton3
-            // 
-            resources.ApplyResources(this.radioButton3, "radioButton3");
-            this.radioButton3.Name = "radioButton3";
-            this.radioButton3.TabStop = true;
-            this.radioButton3.UseVisualStyleBackColor = true;
-            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
-            // 
-            // LoginForm
-            // 
-            resources.ApplyResources(this, "$this");
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Moccasin;
-            this.Controls.Add(this.radioButton3);
-            this.Controls.Add(this.radioButton2);
-            this.Controls.Add(this.radioButton1);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.LoginBT);
-            this.Controls.Add(this.ShowPasswordCB);
-            this.Controls.Add(this.PasswordTB);
-            this.Controls.Add(this.UserNameTB);
-            this.Controls.Add(this.pictureBox3);
-            this.Controls.Add(this.pictureBox2);
-            this.Name = "LoginForm";
-            this.Load += new System.EventHandler(this.LoginForm_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            this.ResumeLayout(false);
-            this.PerformLayout();
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
+			LoginBT = new Button();
+			ShowPasswordCB = new CheckBox();
+			PasswordTB = new TextBox();
+			UserNameTB = new TextBox();
+			pictureBox3 = new PictureBox();
+			pictureBox2 = new PictureBox();
+			label1 = new Label();
+			((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+			((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+			SuspendLayout();
+			// 
+			// LoginBT
+			// 
+			LoginBT.BackColor = SystemColors.ActiveCaptionText;
+			resources.ApplyResources(LoginBT, "LoginBT");
+			LoginBT.ForeColor = SystemColors.ButtonHighlight;
+			LoginBT.Name = "LoginBT";
+			LoginBT.UseVisualStyleBackColor = false;
+			LoginBT.Click += LoginBT_Click_1;
+			// 
+			// ShowPasswordCB
+			// 
+			resources.ApplyResources(ShowPasswordCB, "ShowPasswordCB");
+			ShowPasswordCB.Name = "ShowPasswordCB";
+			ShowPasswordCB.UseVisualStyleBackColor = true;
+			// 
+			// PasswordTB
+			// 
+			PasswordTB.ForeColor = Color.DarkGray;
+			resources.ApplyResources(PasswordTB, "PasswordTB");
+			PasswordTB.Name = "PasswordTB";
+			PasswordTB.UseSystemPasswordChar = true;
+			// 
+			// UserNameTB
+			// 
+			UserNameTB.ForeColor = Color.DarkGray;
+			resources.ApplyResources(UserNameTB, "UserNameTB");
+			UserNameTB.Name = "UserNameTB";
+			// 
+			// pictureBox3
+			// 
+			resources.ApplyResources(pictureBox3, "pictureBox3");
+			pictureBox3.Name = "pictureBox3";
+			pictureBox3.TabStop = false;
+			// 
+			// pictureBox2
+			// 
+			resources.ApplyResources(pictureBox2, "pictureBox2");
+			pictureBox2.Name = "pictureBox2";
+			pictureBox2.TabStop = false;
+			// 
+			// label1
+			// 
+			resources.ApplyResources(label1, "label1");
+			label1.Name = "label1";
+			// 
+			// LoginForm
+			// 
+			resources.ApplyResources(this, "$this");
+			AutoScaleMode = AutoScaleMode.Font;
+			BackColor = Color.Moccasin;
+			Controls.Add(label1);
+			Controls.Add(LoginBT);
+			Controls.Add(ShowPasswordCB);
+			Controls.Add(PasswordTB);
+			Controls.Add(UserNameTB);
+			Controls.Add(pictureBox3);
+			Controls.Add(pictureBox2);
+			Name = "LoginForm";
+			Load += LoginForm_Load;
+			((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+			((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+			ResumeLayout(false);
+			PerformLayout();
+		}
 
-        }
+		#endregion
 
-        #endregion
-
-        private Button LoginBT;
-        private CheckBox ShowPasswordCB;
-        private TextBox PasswordTB;
-        private TextBox UserNameTB;
-        private PictureBox pictureBox3;
-        private PictureBox pictureBox2;
-        private Label label1;
-        private RadioButton radioButton1;
-        private RadioButton radioButton2;
-        private RadioButton radioButton3;
-    }
+		private Button LoginBT;
+		private CheckBox ShowPasswordCB;
+		private TextBox PasswordTB;
+		private TextBox UserNameTB;
+		private PictureBox pictureBox3;
+		private PictureBox pictureBox2;
+		private Label label1;
+	}
 }
